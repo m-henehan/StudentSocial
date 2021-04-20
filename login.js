e2faf81afc112e1767bf1a90a62b12c398d09407
emmau678 <77412390+emmau678@users.noreply.github.com>
2021-04-20 09:46:28 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,20 @@
+function login()
+{
+    let email = document.getElementById('logEmail').value
+    let password = document.getElementById('logPsw').value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            document.cookie = "accessToken=" + user.za;
+            document.cookie = "uid=" + user.uid;
+            console.log("User object", user);
+            window.location.href = "https://webapp0-17967.web.app/homepage.html";
+            // ...
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+        });
+}
