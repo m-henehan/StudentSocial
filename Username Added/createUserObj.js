88bd532ddd581c988de6d11fde1b0e198bbbdc74
Maria Henehan <57994037+m-henehan@users.noreply.github.com>
2021-04-26 18:23:48 +0100

    Create createUserObj.js

---
+++ Username Added/createUserObj.js
@@ -0,0 +1,25 @@
+function createUserObj(){
+    alert("createUserObj called");
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://us-central1-student-social-nuig.cloudfunctions.net/createUserObj', true);
+
+    xhr.setRequestHeader("Content-type", "application/json");
+    //Track the state changes of the request
+    xhr.onreadystatechange = function()
+    {
+        let DONE = 4; //readyState 4 means the request is done
+        let OK = 200; // status 200 is a successful return
+        if (xhr.readyState === DONE) {
+            if (xhr.status === OK) {
+                let docId = JSON.parse(xhr.responseText);
+                document.cookie = "docid =" + docId;
+                //window.location.href = "./joinForumSearchbar.html";
+            } else {
+                console.log('Error: ' + xhr.status);
+            }
+        }
+    };
+    // alert("details:  email: "+ email + "password: "+ password+ "uid: "+  uid);
+    xhr.send(JSON.stringify({"email":document.getElementById("regEmail").value, "password":document.getElementById("regPassword1").value,
+	"username":document.getElementById("regUsername").value, "uid" : getCookie('uid')}));
+}
